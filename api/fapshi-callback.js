f32a5ca19920485a3a8a858369bf06d0991a7d10
Pepsin <ndibetrand201@gmail.com>
2025-11-12 15:20:40 +0100

    added vecel.json

--- api/fapshi-callback.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res) => {
-  const { transId, pendingId } = req.query;
-
-  if (!transId || !pendingId) {
-    return res.status(400).send('Missing parameters');
-  }
-
-  res.redirect(302, `myapp://fapshi-callback?transId=${transId}&pendingId=${pendingId}`);
-};
